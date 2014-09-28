b1a622fc390e17facb017f9e71f1d227fafe0f80
wm <wmxk007@vista.aero>
2014-09-28 21:54:47 +0800

    first_commit

---
+++ README.md
@@ -0,0 +1,3 @@
+cm-11.0 device tree for HTC One XT (endeavortd)
+
+Only recovery can work.
---
+++ cm.mk
@@ -0,0 +1,25 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := endeavortd
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/htc/endeavortd/device_endeavortd.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := endeavortd
+PRODUCT_NAME := cm_endeavortd
+PRODUCT_BRAND := htc
+PRODUCT_MODEL := endeavortd
+PRODUCT_MANUFACTURER := htc
